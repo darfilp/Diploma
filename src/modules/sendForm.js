c161ecf9cc8abe07280279cf5d910cb9ad59b881
Daria <wwdarcool@yandex.ru>
2022-04-12 08:09:23 +0300

    fix comment

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -8,17 +8,17 @@ const sendForm = () => {
     const validate = (list) => {
         let success = true;
 
-        // list.forEach(input => {
-        //     if(input.name === "fio" && (/[^а-яА-Я ]/g.test(input.value) || input.value.length < 2)){
-        //         alert("Правильно введите имя");
-        //     success = false;
-        //     }
+        list.forEach(input => {
+            if(input.name === "fio" && (/[^а-яА-Я ]/g.test(input.value) || input.value.length < 2)){
+                alert("Правильно введите имя");
+            success = false;
+            }
       
-        //     if (input.name === "tel" && (!(/^[\+]{1}([0-9]{11})+$/g.test(input.value)) || input.value === "")){
-        //         alert("Правильно введите телефон");
-        //     success = false;
-        //     }
-        // })
+            if (input.name === "tel" && (!(/^[\+]{1}([0-9]{11})+$/g.test(input.value)) || input.value === "")){
+                alert("Правильно введите телефон");
+            success = false;
+            }
+        })
 
         return success
     };
